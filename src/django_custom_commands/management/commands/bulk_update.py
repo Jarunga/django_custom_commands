5a66fe0a71e24c39b033733a9f1d5a5d5b174836
Jarunga <president.sute@gmail.com>
2023-09-21 14:52:11 +0900

    [fix]target.UPDATE_FIELD_NAMEではなく、UPDATE_FIELD_NAME

--- src/django_custom_commands/management/commands/bulk_update.py
+++ src/django_custom_commands/management/commands/bulk_update.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import os
 from .mixins import CommandMixin,Target
 from django.utils import timezone
+from .settings import UPDATE_FIELD_NAME
 
 # BaseCommandを継承して作成
 class Command(CommandMixin,BaseCommand):
@@ -13,6 +14,6 @@ class Command(CommandMixin,BaseCommand):
 
     def main(self,target):
 
-        target.model.objects.bulk_update(target.data_list,fields=[field for field in target.df.columns if field!=target.model._meta.pk.name]+[target.UPDATE_FIELD_NAME],batch_size=10000)
+        target.model.objects.bulk_update(target.data_list,fields=[field for field in target.df.columns if field!=target.model._meta.pk.name]+[UPDATE_FIELD_NAME],batch_size=10000)
  
         print("更新が完了しました。")
